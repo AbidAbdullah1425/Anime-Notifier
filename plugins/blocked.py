7e5f17c21e6d62d7dc0c80165f80686a24863fa8
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-04-05 13:28:33 +0600

    Update blocked.py

--- plugins/blocked.py
+++ plugins/blocked.py
@@ -1,6 +1,8 @@
 # plugins/blocked.py
-from database import block_anime
+from database import block_anime, unblock_anime
 
 def handle_blocked_command(anime_name):
     block_anime(anime_name)
-    print(f"Anime '{anime_name}' has been blocked.")
+
+def handle_unblocked_command(anime_name):
+    unblock_anime(anime_name)
