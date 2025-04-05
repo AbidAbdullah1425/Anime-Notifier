d2800fd4afb38025291f4d5a910657c259fae200
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-04-05 13:19:04 +0600

    Update notifier.py

--- plugins/notifier.py
+++ plugins/notifier.py
@@ -1,9 +1,8 @@
 # plugins/notifier.py
 import requests
+from config import TG_BOT_TOKEN, OWNER_ID
 
-TELEGRAM_API_URL = 'https://api.telegram.org/bot{}/sendMessage'
-TELEGRAM_BOT_TOKEN = 'your_telegram_bot_token'  # Replace with your bot token
-OWNER_ID = 'your_owner_id'  # Replace with the owner's Telegram ID
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
 
 def send_notification(anime):
     message = f"""
@@ -16,11 +15,10 @@ def send_notification(anime):
     ━━━━━━━━━━━━━━━
     """
     
-    url = TELEGRAM_API_URL.format(TELEGRAM_BOT_TOKEN)
     payload = {
         'chat_id': OWNER_ID,
         'text': message
     }
     
-    response = requests.post(url, data=payload)
+    response = requests.post(TELEGRAM_API_URL, data=payload)
     response.raise_for_status()
