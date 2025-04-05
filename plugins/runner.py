af64506e388797c2910ca8915db52ece7f3e1740
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-04-05 14:54:09 +0600

    Update runner.py

--- plugins/runner.py
+++ plugins/runner.py
@@ -2,8 +2,9 @@
 import time
 import schedule
 from plugins.anilist import fetch_anime_schedule
-from plugins.notifier import send_notification, send_daily_schedule
+from plugins.notifier import send_notification, send_daily_schedule, send_startup_schedule
 from database.database import is_blocked
+from datetime import datetime
 
 def check_anime_updates():
     anime_list = fetch_anime_schedule()
@@ -24,7 +25,14 @@ def send_daily_anime_list():
     anime_list = fetch_anime_schedule()
     send_daily_schedule(anime_list)
 
+def send_startup_anime_list():
+    anime_list = fetch_anime_schedule()
+    send_startup_schedule(anime_list)
+
 if __name__ == '__main__':
+    # Send the startup anime list
+    send_startup_anime_list()
+
     # Schedule the daily anime list at 5 AM
     schedule.every().day.at("05:00").do(send_daily_anime_list)
 
