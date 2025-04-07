8bc7514f58b1fb8fa4b315d956209b13eabc59e4
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-04-07 20:53:15 +0600

    Update anilist.py

--- plugins/anilist.py
+++ plugins/anilist.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import requests
-from bot Import Bot
+from bot import Bot
 import logging
 from config import OWNER_ID
 
