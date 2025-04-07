5e4afa5e2247b0bb3337e6b00ffb4a3b1dd8d759
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-04-07 18:55:30 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ import pyrogram.utils
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, PORT
 
 
-# Import the handlers from anilist_new.py
-from plugins.anilist_new import anime_new_handler, add_button_handler, button_input_handler, done_handler
+# Import the handlers from source.py
+from plugins.spurce import anime_new_handler, add_button_handler, button_input_handler, done_handler
 
 # Import the original handlers from anilist.py
 from plugins.anilist import anime_handler, add_button_handler as add_button_handler_old, button_input_handler as button_input_handler_old, done_handler as done_handler_old
